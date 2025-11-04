362d6d6427d968ffeee2704329d4fe7479618382
ghgus1450-khh <ghgus1450@gmail.com>
2025-11-04 17:31:07 +0900

    1104Fush

--- mypage/src/pages/Contect.jsx
+++ mypage/src/pages/Contect.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 
 export default function Contect (){
+    
+    const [name, setName] = useState();
+
     return(
         <>
-            <h1>Contect 창</h1>
+            <h3>Contect 창</h3>
             <p>이름 : 강호현 <br /> 예쁜나이 스물다섯살 - 1</p>
+            {/* <input type="text" value={} onChange={} /> */}
             <Link to={'/'} className="HomeBtn">홈으로</Link>
         </>
     );

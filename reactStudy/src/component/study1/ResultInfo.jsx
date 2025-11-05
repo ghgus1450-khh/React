cffae1e8751b7d6235502799b3f1f255ff7b39af
ghgus1450-khh <ghgus1450@gmail.com>
2025-11-05 13:03:24 +0900

    1106up

---
+++ reactStudy/src/component/study1/ResultInfo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function ResultInfo(props){
+    
+    return(
+        <>
+            <div>
+                <p>이름 - {props.userName} </p>
+                <p>나이 - {props.userAge} </p>
+                <p>이메일 - {props.userEmail} </p>
+            </div>
+        
+        </>
+    );
+}
